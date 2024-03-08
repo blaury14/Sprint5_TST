108c3daa8cad95e50e3967e5affc31138d139923
blaury14 <blaury14@gmail.com>
2024-03-08 17:29:21 -0300

    Ver B

--- app.py
+++ app.py
@@ -2,23 +2,31 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+# Leer el archivo CSV del conjunto de datos en un DataFrame
 car_data = pd.read_csv('vehicles_us.csv')
 
-# Añade un encabezado
-st.header('Panel de control de anuncios de venta de coches')
+# Crear una casilla de verificación para construir un histograma
+build_histogram = st.checkbox('Construir un histograma')
 
-# Crea un botón para construir un histograma
-hist_button = st.button('Construir histograma')
-
-if hist_button: # Si se hace clic en el botón
-    st.write('Construcción de un histograma para el conjunto de datos de anuncios de venta de coches')
+if build_histogram: # Si la casilla de verificación está seleccionada
+    # Escribir un mensaje
+    st.write('Construir un histograma para la columna odómetro')
+    
+    # Crear un histograma
     fig = px.histogram(car_data, x="odometer")
+    
+    # Mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-# Agrega otro botón para construir un gráfico de dispersión
-scatter_button = st.button('Construir gráfico de dispersión')
+# Crear una casilla de verificación para construir un gráfico de dispersión
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
 
-if scatter_button: # Si se hace clic en el botón
-    st.write('Construcción de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
-    fig = px.scatter(car_data, x="year", y="price", color="manufacturer")
+if build_scatter: # Si la casilla de verificación está seleccionada
+    # Escribir un mensaje
+    st.write('Construir un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    # Crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="year", y="price", color="fuel")
+    
+    # Mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
